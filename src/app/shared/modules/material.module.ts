0997f359d8396fe2b786a56ed34f16dc9c1c902a
amatov.beksultan <beksultan.amatov@timelysoft.net>
2024-04-28 11:27:07 +0600

    set up modal dialog

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from "@angular/core";
 import { MatDialogModule } from "@angular/material/dialog";
+import { MatTableModule } from "@angular/material/table";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSortModule } from "@angular/material/sort";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
-  exports: [MatDialogModule],
+  exports: [
+    MatDialogModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSortModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
 })
 export class MaterialModule {}
--- src/app/shared/modules/standalone-component.module.ts
+++ src/app/shared/modules/standalone-component.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
-
-
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   exports: [
-    MaterialModule
+    MaterialModule,
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class StandaloneComponentModule { }
